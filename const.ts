d01645ca89bfabf6b3c57232d056c9a2305286ee
Ba <BA>
2025-10-18 00:43:11 +0700

    Fix

--- const.ts
+++ const.ts
@@ -1,3 +1,3 @@
 
 export const SPIDERMESH_NAMESPACE = process.env.SPIDERMESH_NAMESPACE || 'default'
-export const SPIDERMESH_NODE_HOSTNAME = process.env.SPIDERMESH_NODE_HOSTNAME || 'localhost'
+export const SPIDERMESH_NODE_HOSTNAME = process.env.SPIDERMESH_NODE_HOSTNAME || ''
